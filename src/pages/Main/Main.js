53ab4cbac0f7a8c4b98f2bc2746b58a983e011f3
youngseoKim-kr <ysk5754@naver.com>
2022-05-11 11:01:07 +0900

    Add : Main 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
 function Main() {
-  return <div>set</div>;
+  return (
+    <MainSection>
+      <MainTitle>입출금</MainTitle>
+    </MainSection>
+  );
 }
+const MainSection = styled.section`
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+  height: 700px;
+`;
+
+const MainTitle = styled.span`
+  color: #6658d3;
+  font-size: 24px;
+  font-weight: 600;
+`;
 
 export default Main;
