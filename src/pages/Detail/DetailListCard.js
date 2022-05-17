705606aa78c58edb6c9ab272874d7644511683cb
youngseoKim-kr <ysk5754@naver.com>
2022-05-17 16:12:14 +0900

    Add : fetch 부분 추가

--- src/pages/Detail/DetailListCard.js
+++ src/pages/Detail/DetailListCard.js
@@ -1,14 +1,31 @@
 import styled from 'styled-components';
 
 function DetailListCard(props) {
+  const ValuationAmount = (props.price * props.quantity)
+    .toString()
+    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+    .substr(0, 15);
+
+  const holdingQuantity = props.quantity
+    .toString()
+    .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
+    .substr(0, 15);
+
   return (
     <TableList>
       <td>{props.division}</td>
       <td>{props.name}</td>
       <td>{props.type}</td>
-      <td>{props.quantity}</td>
-      <td>₩ {props.price}</td>
-      <td>{props.status}</td>
+      <td>{holdingQuantity}</td>
+      <td>₩ {ValuationAmount}</td>
+      <td>
+        {props.status} <br />
+        {props.status === '진행' || props.status === '대기' ? (
+          <button>취소</button>
+        ) : (
+          ''
+        )}
+      </td>
       <td className="address">{props.address}</td>
       <td>{props.create}</td>
     </TableList>
@@ -22,6 +39,14 @@ const TableList = styled.tr`
     border-bottom: 1px solid black;
     text-align: center;
     vertical-align: middle;
+    button {
+      width: 30px;
+      border: none;
+      color: white;
+      background-color: red;
+      font-size: 9px;
+      cursor: pointer;
+    }
   }
   .address {
     word-wrap: break-word;
